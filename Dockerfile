ced0c961d5da3f4fb923e3b1e6d096f8ceb470fa
ML <ml.blue@icloud.com>
2024-04-16 21:13:22 +0200

    added dotenv and updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -13,5 +13,5 @@ RUN pip install -r requirements.txt
 
 # Docker Run Command
 EXPOSE 5000
-ENV FLASK_APP=flask_app.py
+ENV FLASK_APP=app.py
 CMD [ "python", "-m" , "flask", "run", "--host=0.0.0.0"]
--- chatbot/openai.py
+++ chatbot/openai.py
@@ -1,4 +1,7 @@
-import os
+from dotenv import load_dotenv
 
-OPENAI_KEY = os.environ['OPENAI_KEY']
+load_dotenv()  # load environment variables from .env file
+
+import os
+OPENAI_KEY = os.getenv("OPENAI_KEY")
 OPENAI_MODEL = "gpt-3.5-turbo-16k"
